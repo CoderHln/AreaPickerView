3b3a0f7f0039165a67401175feeb88ce9e86c0c3
林楠 <hanlinnan@sy8.com>
2018-12-28 10:20:07 +0800

    Initial Commit

---
+++ AreaPickView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AreaPickView
+//
+//  Created by SY8 on 2018/12/28.
+//  Copyright © 2018年 SY8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AreaPickView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AreaPickView
+//
+//  Created by SY8 on 2018/12/28.
+//  Copyright © 2018年 SY8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
